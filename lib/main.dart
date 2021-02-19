67490a2bc36bc5416f323fc8cb1165af3d5c76e0
Michael Fernandez <mikevfern@gmail.com>
2021-02-18 18:29:55 -0800

    wired up graphql_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:eop_mobile/pages/LoginPage.dart';
 import 'package:eop_mobile/pages/RegisterPage.dart';
 
--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
 import 'package:eop_mobile/components/CredentialsInput.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -20,48 +13,101 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  bool _loggedIn = false;
+  Color primaryThemeColor = Color(0xFFFF1155);
+
+  @override
+  void initState() {
+    super.initState();
+
+    print('Init state!');
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        toolbarHeight: 30.0,
-        title: Text(widget.title),
+    final HttpLink httpLink = HttpLink(
+      'https://eop-backend-evtm3.ondigitalocean.app/backend/',
+    );
+
+    String login = """
+            mutation {
+                login (
+                    email: "user.test.two2@email.com",
+                    password: "testPassword123"
+                ){
+                    token
+                }
+            }
+    """;
+
+    ValueNotifier<GraphQLClient> client = ValueNotifier(
+      GraphQLClient(
+        link: httpLink,
+        // The default store is the InMemoryStore, which does NOT persist to disk
+        cache: GraphQLCache(),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/images/eop_logo.png'),
-                backgroundColor: Colors.pink,
-                radius: 40.0,
-              ),
-            ),
-            CredentialsInput(label: 'email'),
-            CredentialsInput(label: 'password'),
-            RaisedButton(
-              onPressed: () {
-                print('Log In button pressed.');
-              },
-              child: Text('Log In'),
-            ),
-            FlatButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/register');
-              },
-              child: Text(
-                'Register',
-                style: TextStyle(
-                  decoration: TextDecoration.underline,
+    );
+
+    return GraphQLProvider(
+      client: client,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          toolbarHeight: 30.0,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/eop_logo.png'),
+                  backgroundColor: primaryThemeColor,
+                  radius: 40.0,
                 ),
               ),
-            ),
-          ],
+              CredentialsInput(label: 'email'),
+              CredentialsInput(label: 'password'),
+              RaisedButton(
+                color: primaryThemeColor,
+                onPressed: () {
+                  print('Log In button pressed.');
+                },
+                child: Text('Log In'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/register');
+                },
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ),
+              Query(
+                options: QueryOptions(
+                  document: gql(login),
+                ),
+                builder: (QueryResult result,
+                    {VoidCallback refetch, FetchMore fetchMore}) {
+                  if (result.hasException) {
+                    return Text(result.exception.toString());
+                  }
+
+                  if (result.isLoading) {
+                    return Text('...loading');
+                  }
+
+                  return Text(
+                    result.data['login']['token'].toString(),
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
